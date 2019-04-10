61df5ef8d2f14b607b53bd14a219287caeb984f9
JakeRoeleven <jakeroeleven44@gmail.com>
2019-04-10 14:03:08 +1000

    Database retrival working

--- app/src/main/java/team7/voluntime/Fragments/Common/UserProfileFragment.java
+++ app/src/main/java/team7/voluntime/Fragments/Common/UserProfileFragment.java
@@ -1,17 +1,32 @@
 package team7.voluntime.Fragments.Common;
 
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseUserMetadata;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import team7.voluntime.Activities.LoginActivity;
@@ -23,7 +38,29 @@ import team7.voluntime.R;
  */
 public class UserProfileFragment extends Fragment {
 
-    private final static String TAG = "MapFragment";
+    private final static String TAG = "UserProfileFragment";
+    private String userFname;
+    private String Type;
+    private String UserName;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private FirebaseUser mUser;
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
+
+    private static String accountType;
+
+
+    //Bindings
+    @BindView(R.id.uName)
+    TextView uName;
+
+    @BindView(R.id.uType)
+    TextView uType;
+
+    @BindView(R.id.uEmail)
+    TextView uEmail;
 
 
     public UserProfileFragment() {
@@ -32,11 +69,42 @@ public class UserProfileFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_user_profile, container, false);
         ButterKnife.bind(this, v);
+
+        //Charity or Volunteer
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+
+        //Shit design
+        DatabaseReference typeRef = FirebaseDatabase.getInstance().getReference("Volunteers");
+
+        typeRef.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snap : dataSnapshot.getChildren()) {
+                    Type = dataSnapshot.child("AccountType").getValue().toString();
+                    UserName = dataSnapshot.child("Profile").child("FullName").getValue().toString();
+                    uName.setText(UserName);
+                    uType.setText(Type);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+
+        uType.setText("None");
+        uEmail.setText(mUser.getEmail());
+
+        uName.setText("None");
         return v;
     }
 
