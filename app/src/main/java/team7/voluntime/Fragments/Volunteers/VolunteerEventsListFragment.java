9c7e207438aa900782650a500fe1f5322fa1df67
Jahmilli <sebastian.southern@gmail.com>
2019-05-01 18:20:08 +1000

    displaying all events for volunteer

---
+++ app/src/main/java/team7/voluntime/Fragments/Volunteers/VolunteerEventsListFragment.java
@@ -0,0 +1,188 @@
+package team7.voluntime.Fragments.Volunteers;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import team7.voluntime.Activities.CreateEventActivity;
+import team7.voluntime.Domains.Charity;
+import team7.voluntime.Domains.Event;
+import team7.voluntime.Domains.EventVolunteers;
+import team7.voluntime.Domains.Volunteer;
+import team7.voluntime.Fragments.Charities.ViewEventsFragment;
+import team7.voluntime.R;
+import team7.voluntime.Utilities.EventListAdapter;
+import team7.voluntime.Utilities.Utilities;
+
+
+public class VolunteerEventsListFragment extends Fragment {
+    FirebaseUser mUser;
+    private FirebaseDatabase database;
+    private DatabaseReference volunteerReference;
+    private DatabaseReference eventsReference;
+    private Volunteer volunteer;
+
+    ListView listOfUpcomingEvents;
+
+    @BindView(R.id.volunteerUpcomingEventsTV)
+    TextView upcomingEventsTV;
+
+    @BindView(R.id.volunteerViewEventTitleTV)
+    TextView viewEventTitleTV;
+
+    @BindView(R.id.volunteerCreateEventIV)
+    ImageView createEventIV;
+
+    private final static String TAG = "VolunteerEventsList";
+
+
+    public VolunteerEventsListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getActivity().setTitle("All Events");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_volunteer_events_list, container, false);
+        ButterKnife.bind(this, v);
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        volunteerReference = Utilities.getVolunteerReference(database, mUser.getUid());
+        eventsReference = Utilities.getEventsReference(database);
+
+        listOfUpcomingEvents = (ListView) v.findViewById(R.id.volunteerListOfUpcomingEventsLV);
+
+        final ArrayList<Event> upcomingEventList = new ArrayList<>();
+
+        EventListAdapter upcomingEventListAdapter = new EventListAdapter(
+                getActivity(),
+                R.layout.adapter_view_event_layout,
+                upcomingEventList,
+                VolunteerEventsListFragment.this);
+
+        listOfUpcomingEvents.setAdapter(upcomingEventListAdapter);
+
+        // Attach a listener to read the data at our posts reference
+        volunteerReference.child("Profile").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                volunteer = dataSnapshot.getValue(Volunteer.class);
+                volunteer.setId(mUser.getUid());
+                Log.d(TAG, volunteer.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "The read failed: " + databaseError.getCode());
+            }
+        });
+
+        eventsReference.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        upcomingEventList.clear();
+                        upcomingEventsTV.setVisibility(View.VISIBLE);
+
+                        for (DataSnapshot child : dataSnapshot.getChildren()) {
+                            if (child.exists()) {
+                                String eventId = child.getKey();
+                                Log.d(TAG, "Event id is " + eventId);
+
+                                String minimum = (child.child("EventVolunteers").child("minimum").getValue() != null)
+                                        ? child.child("EventVolunteers").child("minimum").getValue().toString() : null;
+                                String maximum = (child.child("EventVolunteers").child("maximum").getValue() != null)
+                                        ? child.child("EventVolunteers").child("maximum").getValue().toString() : null;
+
+                                // TODO: Create pending, registered and attended
+//                                if (child.child("EventVolunteers").child("pendingVolunteers").getValue() != null) {
+//                                    Map map = child.child("EventVolunteers").child("pendingVolunteers").getValue(Map.class);
+//                                    Log.d(TAG, "map is " + map.toString());
+//                                }
+//                                ArrayList<String> pendingVolunteers = (child.child("EventVolunteers").child("pendingVolunteers").getValue() != null)
+//                                        ? child.child("pendingVolunteers").getValue() : null;
+//                                String maximum = (child.child("EventVolunteers").child("maximum").getValue() != null)
+//                                        ? child.child("maximum").getValue().toString() : null;
+//                                String maximum = (child.child("EventVolunteers").child("maximum").getValue() != null)
+//                                        ? child.child("maximum").getValue().toString() : null;
+
+
+                                EventVolunteers eventVolunteers = new EventVolunteers();
+                                int intMin = minimum == null ? 0 : Integer.parseInt(minimum);
+                                int intMax = maximum == null ? 0 : Integer.parseInt(maximum);
+                                eventVolunteers.setMinimum(intMin);
+                                eventVolunteers.setMaximum(intMax);
+
+
+                                Event event = child.getValue(Event.class);
+                                event.setId(eventId);
+                                event.setVolunteers(eventVolunteers);
+
+                                // Will only display events that the charity has createdif (event.getOrganisers().equals(mUser.getUid())) {
+                                Log.d(TAG, event.toString());
+                                upcomingEventList.add(event);
+                                if (upcomingEventsTV.getVisibility() != View.INVISIBLE) {
+                                    upcomingEventsTV.setVisibility(View.INVISIBLE);
+                                }
+                                listOfUpcomingEvents.invalidateViews();
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @OnClick(R.id.volunteerCreateEventIV)
+    public void createEventOnClick() {
+//        Intent createEventIntent = new Intent(getActivity(), CreateEventActivity.class);
+//        createEventIntent.putExtra("id", volunteer.getId());
+//        createEventIntent.putExtra("categories", volunteer.getCategory());
+//        startActivity(createEventIntent);
+        Log.d(TAG, "Event Button Clicked");
+    }
+
+}
