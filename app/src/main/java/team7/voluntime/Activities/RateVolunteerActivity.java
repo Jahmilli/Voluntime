d53c7a7c69d66a995f2f9d776d8e4af82bf05bf1
JakeRoeleven <jakeroeleven44@gmail.com>
2019-06-01 21:49:28 +1000

    Final

--- app/src/main/java/team7/voluntime/Activities/RateVolunteerActivity.java
+++ app/src/main/java/team7/voluntime/Activities/RateVolunteerActivity.java
@@ -6,7 +6,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
@@ -135,6 +134,11 @@ public class RateVolunteerActivity extends AppCompatActivity {
             reference.setValue(rateVolunteer);
             finish();
         }
+    }
+
+    @OnClick(R.id.voleBackTV)
+    public void backButtonTV() {
+        finish();
     }
 
 }
